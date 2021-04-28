ee9fc33e6251b0a99459ddbfaf6fca822060b177
Raoni Padilha Nunes <79607898+RaoniPNunes@users.noreply.github.com>
2021-04-28 20:18:02 -0300

    Department entity class

---
+++ src/model/entities/Department.java
@@ -0,0 +1,67 @@
+
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+
+public class Department implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    private Integer Id;
+    private String Name;
+
+    public Department(Integer Id, String Name) {
+        this.Id = Id;
+        this.Name = Name;
+    }
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer Id) {
+        this.Id = Id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 41 * hash + Objects.hashCode(this.Id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Department other = (Department) obj;
+        if (!Objects.equals(this.Id, other.Id)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" + "Id=" + Id + ", Name=" + Name + '}';
+    }
+    
+    
+    
+}
