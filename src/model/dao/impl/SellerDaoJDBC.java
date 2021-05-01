3e32a3d2142261df99bf0ed3ed432c1c47821986
Raoni Padilha Nunes <79607898+RaoniPNunes@users.noreply.github.com>
2021-05-01 11:22:45 -0300

    Inclusão das classe SellerDaoJDBC e DaoFactory

---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,34 @@
+
+package model.dao.impl;
+
+import java.util.List;
+import model.dao.SellerDao;
+import model.entities.Sellers;
+
+
+public class SellerDaoJDBC implements SellerDao{
+
+    @Override
+    public void insert(Sellers obj) {
+    }
+
+    @Override
+    public void update(Sellers obj) {
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+    }
+
+    @Override
+    public Sellers findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Sellers> findAll() {
+        return null;
+    }
+    
+    
+}
