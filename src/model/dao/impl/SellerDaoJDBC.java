2df1eccf615f0b8976c5b1e015f3e0da771dc6b8
Raoni Padilha Nunes <79607898+RaoniPNunes@users.noreply.github.com>
2021-05-05 09:10:54 -0300

    Desenvolvimento do método Insert.

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -20,6 +20,38 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public void insert(Sellers obj) {
+        PreparedStatement st = null;
+        
+        try{
+            st = con.prepareStatement("INSERT INTO sellers "
+                                     +"(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+                                     +"VALUES "
+                                     +"(?, ?, ?, ?, ?) ");
+            
+            st.setString(1, obj.getName());
+            st.setString(2, obj.getEmail());
+            st.setDate(3, new java.sql.Date(obj.getBirthdate().getTime()));
+            st.setDouble(4, obj.getBasesalary());
+            st.setInt(5, obj.getDepartment().getId());
+            
+            int linhasAfetadas = st.executeUpdate();
+            
+            if (linhasAfetadas >0){
+                ResultSet rs = st.getGeneratedKeys();
+                
+                if (rs.next()){
+                    int id = rs.getInt(1);
+                    obj.setId(id);
+                }
+            }
+            else{
+                throw new DbException("Erro inesperado. Nenhuma linha afetada");
+            }
+                        
+        }
+        catch(SQLException e){
+            throw new DbException(e.getMessage());
+        }
     }
 
     @Override
